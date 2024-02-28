85c33b1b035a264621842c2a8cbc8aba281ee8d7
Jakob Varenbud <103386301+jakob-varenbud@users.noreply.github.com>
2024-02-28 11:27:49 +0100

    changed mapbox style to be responsive

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { initializeMapbox } from '$utils/Mapbox.ts';
-
 import { startSequentielleAnimation } from './utils/gsap';
 import { siegelStagger } from './utils/gsap';
 import { carousel } from './utils/gsap';
 import { timeline } from './utils/gsap';
 import { sustainableSection } from './utils/gsap';
+import { initializeMapbox } from './utils/Mapbox.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
   startSequentielleAnimation();
--- src/utils/Mapbox.ts
+++ src/utils/Mapbox.ts
@@ -1,5 +1,3 @@
-import 'src/style.css';
-//Mapbox
 export const initializeMapbox = () => {
   mapboxgl.accessToken =
     'pk.eyJ1IjoiamFrb2J2YXIiLCJhIjoiY2xyeXlxa2w5MWtxYzJqbmFmZXJ4MXRhZiJ9.TxpTMaCXvDgq5akCtF1kZQ';
@@ -10,13 +8,8 @@ export const initializeMapbox = () => {
 
   const map = new mapboxgl.Map({
     container: 'mapbox',
-    style: 'mapbox://styles/jakobvar/clsltaizq001l01pk0511f7ne',
-    zoom: 1.5, // Standard-Zoom-Leve
-    center: [0, 10], // center default
-    minZoom: 1.5, // Minimal erlaubter Zoom-Level, um aus der Ansicht der ganzen Welt nicht weiter herauszuzoomen
-    dragPan: false, // Deaktiviert das Verschieben der Karte
-    scrollZoom: true, // Optional: deaktiviert das Zoomen durch Scrollen, für vollständige Kontrolle
-    touchZoomRotate: true, // Deaktiviert Zoom und Rotation auf Touch-Geräten
+    style: 'mapbox://styles/jakobvar/cls0hl1nj00z501r42kewbyqu',
+    zoom: 1, // Standard-Zoom-Leve
   });
 
   const locations = [
@@ -25,27 +18,27 @@ export const initializeMapbox = () => {
       info: 'Informationen über Australien',
       googleMapsLink: 'https://www.google.com/maps?q=-25.2744,133.7751',
       image:
-        'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
+        'https://uploads-ssl.webflow.com/655f64b940de048ead148f50/65b3f70072f408c610863c11_MapPin.png',
     }, // Australien
     {
       coordinates: [22.9375, -30.5595],
       image:
-        'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
+        'https://uploads-ssl.webflow.com/655f64b940de048ead148f50/65b3f70072f408c610863c11_MapPin.png',
     }, // Südafrika
     {
       coordinates: [104.1954, 35.8617],
       image:
-        'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
+        'https://uploads-ssl.webflow.com/655f64b940de048ead148f50/65b3f70072f408c610863c11_MapPin.png',
     }, // China
     {
       coordinates: [-102.5528, 23.6345],
       image:
-        'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
+        'https://uploads-ssl.webflow.com/655f64b940de048ead148f50/65b3f70072f408c610863c11_MapPin.png',
     }, // Mexiko
     {
       coordinates: [-95.7129, 37.0902],
       image:
-        'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
+        'https://uploads-ssl.webflow.com/655f64b940de048ead148f50/65b3f70072f408c610863c11_MapPin.png',
     }, // USA
   ];
 
@@ -54,8 +47,8 @@ export const initializeMapbox = () => {
     const el = document.createElement('div');
     el.className = 'custom-marker';
     el.style.backgroundImage = `url('${loc.image}')`;
-    el.style.width = isMobileDevice() ? '50px' : '50px';
-    el.style.height = isMobileDevice() ? '50px' : '50px';
+    el.style.width = isMobileDevice() ? '50px' : '85px';
+    el.style.height = isMobileDevice() ? '50px' : '85px';
     el.style.backgroundSize = 'cover';
     el.style.position = 'absolute';
     el.style.transform = 'translate(-50%, -100%)';
@@ -63,8 +56,7 @@ export const initializeMapbox = () => {
 
     // Erstelle das Popup
     const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(`
-            <h6>DRS Group</h6>
-            <p class='popup-p'> Am Sandtorkai 58, D-20457 HamburgEmail: info@drs-group.com </p>
+            <p>${loc.info}</p>
             <a href="${loc.googleMapsLink}" target="_blank">In Google Maps anzeigen</a>
         `);
 
@@ -73,5 +65,19 @@ export const initializeMapbox = () => {
       .setLngLat(loc.coordinates)
       .setPopup(popup)
       .addTo(map);
+  });
+  map.on('load', function () {
+    // Passt die Kartenansicht an, um alle Marker einzuschließen
+    const bounds = locations.reduce(
+      function (bounds, loc) {
+        return bounds.extend(loc.coordinates);
+      },
+      new mapboxgl.LngLatBounds(locations[0].coordinates, locations[0].coordinates)
+    );
+
+    map.fitBounds(bounds, {
+      padding: 50, // Optionaler Abstand von den Rändern der Karte in Pixeln
+      maxZoom: isMobileDevice() ? 2 : 3, // Maximaler Zoom-Level
+    });
   });
 };
