617c662e39c2b036fa0ad07a4bb843b25ed3b344
Jakob Varenbud <103386301+jakob-varenbud@users.noreply.github.com>
2024-06-12 15:43:30 +0200

    adjust scrolling behaviour

--- src/home/utils/Mapbox.ts
+++ src/home/utils/Mapbox.ts
@@ -11,33 +11,55 @@ export const initializeMapbox = () => {
     style: 'mapbox://styles/jakobvar/cls0hl1nj00z501r42kewbyqu',
     zoom: 1, // Standard-Zoom-Level
     renderWorldCopies: false, // Verhindert das Anzeigen von Weltkopien
+    minZoom: 1, // Minimum zoom level
+    maxZoom: 1, // Maximum zoom level
   });
+
+  // Disable zooming on the map
+  map.scrollZoom.disable();
+  map.doubleClickZoom.disable();
+  map.boxZoom.disable();
+  map.dragPan.disable();
+  map.dragRotate.disable();
+  map.keyboard.disable();
+  map.touchZoomRotate.disable();
+
+  // Disable scroll interaction
+  map.getCanvas().style.pointerEvents = 'none';
 
   const locations = [
     {
       coordinates: [133.7751, -25.2744],
-      info: 'Informationen über Australien',
-      googleMapsLink: 'https://www.google.com/maps?q=-25.2744,133.7751',
+      info: 'Am Sandtorkai 58, 20457 Hamburg',
+      googleMapsLink: 'https://maps.app.goo.gl/Ep4fwRLKSxLknk3V6',
       image:
         'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
     }, // Australien
     {
       coordinates: [22.9375, -30.5595],
+      info: 'Am Sandtorkai 58, 20457 Hamburg',
+      googleMapsLink: 'https://maps.app.goo.gl/Ep4fwRLKSxLknk3V6',
       image:
         'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
     }, // Südafrika
     {
       coordinates: [104.1954, 35.8617],
+      info: 'Am Sandtorkai 58, 20457 Hamburg',
+      googleMapsLink: 'https://maps.app.goo.gl/Ep4fwRLKSxLknk3V6',
       image:
         'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
     }, // China
     {
       coordinates: [-102.5528, 23.6345],
+      info: 'Am Sandtorkai 58, 20457 Hamburg',
+      googleMapsLink: 'https://maps.app.goo.gl/Ep4fwRLKSxLknk3V6',
       image:
         'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
     }, // Mexiko
     {
       coordinates: [-95.7129, 37.0902],
+      info: 'Am Sandtorkai 58, 20457 Hamburg',
+      googleMapsLink: 'https://maps.app.goo.gl/Ep4fwRLKSxLknk3V6',
       image:
         'https://uploads-ssl.webflow.com/65ca143babb87719f1ed2239/65cccc0f3328ca876509b7a6_Pin.png',
     }, // USA
@@ -67,6 +89,7 @@ export const initializeMapbox = () => {
       .setPopup(popup)
       .addTo(map);
   });
+
   map.on('load', function () {
     // Passt die Kartenansicht an, um alle Marker einzuschließen
     const bounds = locations.reduce(
@@ -78,7 +101,7 @@ export const initializeMapbox = () => {
 
     map.fitBounds(bounds, {
       padding: 50, // Optionaler Abstand von den Rändern der Karte in Pixeln
-      maxZoom: isMobileDevice() ? 2 : 3, // Maximaler Zoom-Level
+      maxZoom: isMobileDevice() ? 1 : 1, // Maximaler Zoom-Level
     });
   });
 };
